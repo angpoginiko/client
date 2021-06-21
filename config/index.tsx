49f5e171fc138d729fb90e4fc2f5ee10c008c133
bglascano <74045856+bglascano@users.noreply.github.com>
2021-06-21 22:04:38 +0800

    Fix urls

--- config/index.tsx
+++ config/index.tsx
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://client-ogwj09pym-angpoginiko.vercel.app/';
+export const server = dev ? 'http://localhost:3000' : 'https://client-angpoginiko.vercel.app/';
