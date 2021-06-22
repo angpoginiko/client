7b5369c52fbb82f5d138bf0147f5d611e1944e63
bglascano <74045856+bglascano@users.noreply.github.com>
2021-06-22 23:57:03 +0800

    Fix Store

---
+++ pages/store.tsx
@@ -0,0 +1,79 @@
+import Layout from '../components/Layout'
+import {
+  Box,
+  Heading,
+  Container,
+  Text,
+  Stack,
+	Icon,
+	Center,
+	useDisclosure
+} from '@chakra-ui/react';
+import { NextPageContext } from 'next';
+import { frontEndAuthentication } from './api/frontEndAuthentication';
+import { server } from '../config';
+import { useEffect } from 'react';
+import userRoles from '../constants/userRoles';
+import { useRouter } from 'next/router';
+import { MdCenterFocusStrong } from 'react-icons/md';
+import { useState } from 'react';
+
+export default function AuthIndexPage({ user, onStore} : any) {
+	const { onOpen, isOpen, onClose } = useDisclosure();
+	const [frontPageClicker, setFrontPageClicker] = useState(false);
+	const router = useRouter();
+	useEffect(() => {
+		if(userRoles.Cashier == user.userRole){
+			router.replace('/HomeCashier')
+		} else if(userRoles.Admin == user.userRole){
+			router.replace('/HomeAdmin')
+		} else if(!onStore){
+			router.replace('./Home');
+		}
+	}, [user, onStore]);
+
+	const onModalClose = () => {
+		setFrontPageClicker(false);
+		onClose();
+	}
+  return (
+    <>
+			<Layout authentication={user} isModalOpen={isOpen} onModalClose={onModalClose} frontPageClick={frontPageClicker} onStore title="Store">
+      <Container maxW={'3xl'}>
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}>
+					<Center>
+					<Box as='button'>
+          	<Icon onClick={()=> {onOpen(), setFrontPageClicker(true)}} as={MdCenterFocusStrong} boxSize="350px"/>
+					</Box>
+					</Center>
+					<Heading
+            fontWeight={600}
+            fontSize={{ base: 'xl', sm: '2xl', md: '4xl' }}
+            lineHeight={'110%'}>
+            Start Scanning <br />
+            <Text as={'span'} color={'green.400'}>
+            </Text>
+          </Heading>
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}>
+          </Stack>
+        </Stack>
+      </Container>
+		</Layout>
+    </>
+  );
+}
+
+AuthIndexPage.getInitialProps = async (ctx: NextPageContext) => {
+	const json = await frontEndAuthentication(`${server}/api/profile/retrieve`, ctx);
+	const onStore = await frontEndAuthentication(`${server}/api/profile/${json!.id}`, ctx);
+	return {user: json, onStore};
+}
